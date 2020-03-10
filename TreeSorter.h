239bb7a1e967d3b63d06d0e36ff2c1e48728db43
Havlong <grishin_evgenii@inbox.ru>
2020-03-10 19:24:15 +0300

    Iteration-based tree sort implementation

--- TreeSorter.h
+++ TreeSorter.h
@@ -3,7 +3,7 @@
  * TreeSorter
  * 
  * @author havlong 
- * @version 1.0
+ * @version 2.0
  */
 
 #ifndef TREESORT_TREESORTER_H
@@ -16,48 +16,34 @@ class TreeSorter {
 private:
     std::vector<int> tree;
     const std::vector<T> &origin;
+    int treeSize;
+    int originSize;
 
-    void build(const int ql, const int qr, const int &pos = 1) {
-        if (ql == qr) {
-            tree[pos] = ql;
-            return;
-        }
-        int mid = (ql + qr) / 2;
-        build(ql, mid, pos * 2);
-        build(mid + 1, qr, pos * 2 + 1);
-        tree[pos] = origin[tree[pos * 2]] < origin[tree[pos * 2 + 1]] ? tree[pos * 2] : tree[pos * 2 + 1];
+    void build() {
+        for (int i = originSize - 1; i > 0; --i)
+            tree[i] = origin[tree[i * 2]] < origin[tree[i * 2 + 1]] ? tree[i * 2] : tree[i * 2 + 1];
     }
 
-    void remove(const int &i, const int ql, const int qr, const int &pos = 1) {
-        if (ql == qr) {
-            tree[pos] = -1;
-            return;
-        }
-        int mid = (ql + qr) / 2;
-        if (tree[pos * 2] == i) {
-            remove(i, ql, mid, pos * 2);
-        } else {
-            remove(i, mid + 1, qr, pos * 2 + 1);
-        }
-        if (tree[pos * 2] == -1) {
-            tree[pos] = tree[pos * 2 + 1];
-        } else if (tree[pos * 2 + 1] == -1) {
-            tree[pos] = tree[pos * 2];
-        } else {
-            tree[pos] = origin[tree[pos * 2]] < origin[tree[pos * 2 + 1]] ? tree[pos * 2] : tree[pos * 2 + 1];
+    void remove(int i) {
+        for (tree[i += originSize] = -1; i > 1; i /= 2) {
+            if (tree[i] == -1)
+                tree[i / 2] = tree[i ^ 1];
+            else if (tree[i ^ 1] == -1)
+                tree[i / 2] = tree[i];
+            else
+                tree[i / 2] = origin[tree[i]] < origin[tree[i ^ 1]] ? tree[i] : tree[i ^ 1];
         }
     }
 
 public:
     explicit TreeSorter(const std::vector<T> &origin) : origin(origin) {
-        int treeSize = 1; // Power of 2 required to contain all of the elements
-        int originSize = origin.size() - 1;
-        while (originSize) {
-            originSize /= 2;
-            treeSize *= 2;
+        originSize = origin.size();
+        treeSize = originSize * 2;
+        tree.resize(treeSize);
+        for (int i = originSize; i < treeSize; ++i) {
+            tree[i] = i - originSize;
         }
-        tree.resize(treeSize * 2);
-        build(0, origin.size() - 1);
+        build();
     }
 
     std::vector<T> sorted() {
@@ -65,7 +51,7 @@ public:
         sorted.reserve(origin.size());
         for (int i = 0; i < origin.size(); ++i) {
             sorted.push_back(origin[tree[1]]);
-            remove(tree[1], 0, origin.size() - 1);
+            remove(tree[1]);
         }
         return sorted;
     }
